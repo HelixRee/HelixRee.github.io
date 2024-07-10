e5b505b47e4aa79bfe25f719caf75763c612de32
HelixRee <subcon888@gmail.com>
2024-07-10 14:51:45 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// Background grid filler
+document.addEventListener('DOMContentLoaded', function () {
+    createBoxes();
+});
+window.addEventListener('resize', function () {
+    // Redo box creation
+    removeBoxes();
+    createBoxes();
+});
+
+function createBoxes() {
+    // Get all instances of bg grid
+    document.querySelectorAll('.background-grid').forEach(function(div) {
+      var container = document.querySelector('.background-grid');
+      var containerWidth = Math.floor(container.offsetWidth / 20),
+          containerHeight = Math.ceil(container.offsetHeight / 20);
+      var squareAmount = containerWidth * containerHeight;
+      for (var i = 0; i < squareAmount; i++) {
+        let box = document.createElement('div');
+        div.appendChild(box);
+        let light = document.createElement('div');
+        light.classList.add("background-grid-light");
+        box.appendChild(light);
+      }
+    });
+  }
+  
+function removeBoxes() {
+    // Get all instances of bg grid
+    document.querySelectorAll('.background-grid').forEach(function(div) {
+        var container = document.querySelector('.background-grid');
+        var containerWidth = Math.floor(container.offsetWidth / 20),
+            containerHeight = Math.ceil(container.offsetHeight / 20);
+        var squareAmount = containerWidth * containerHeight;
+        for (var i = 0; i < squareAmount; i++) {
+          div.querySelectorAll('div').forEach(function(box) {
+            box.remove();
+          });}
+      });
+}
