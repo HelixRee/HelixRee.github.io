f044eff2358adc84091b68f58af43e03b8eb80c0
HelixRee <68953458+HelixRee@users.noreply.github.com>
2024-07-11 10:41:02 +0800

    Fixed touch frfr

--- script.js
+++ script.js
@@ -67,18 +67,19 @@ function createBannerText(displayText) {
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  elmnt.onmousedown = dragMouseDown;
-  elmnt.ontouchstart = dragMouseDown;
+
+  elmnt.addEventListener('mousedown', dragMouseDown);
+  elmnt.addEventListener('touchstart', dragMouseDown);
 
   function dragMouseDown(e) {
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.ontouchend = closeDragElement;
+    document.addEventListener('mouseup', closeDragElement);
+    document.addEventListener('touchend', closeDragElement);
     // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-    document.ontouchmove = elementDrag;
+    document.addEventListener('mousemove', elementDrag);
+    document.addEventListener('touchmove', elementDrag);
   }
 
   function elementDrag(e) {
@@ -94,10 +95,10 @@ function dragElement(elmnt) {
 
   function closeDragElement() {
     // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
+    document.removeEventListener('mouseup', closeDragElement);
+    document.removeEventListener('mousemove', elementDrag);
 
-    document.ontouchend = null;
-    document.ontouchmove = null;
+    document.removeEventListener('touchend', closeDragElement);
+    document.removeEventListener('touchmove', elementDrag);
   }
 }
